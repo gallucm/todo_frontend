17390e10ffac8bc600bf577c444cd59bb3c026c3
Cristian Galluccio <gallucciocristian@gmail.com>
2021-09-23 21:20:42 -0300

    DateUpdated component added

--- src/components/notes/NoteContent.tsx
+++ src/components/notes/NoteContent.tsx
@@ -1,7 +1,8 @@
 import { useDispatch } from "react-redux"
 import { startNoteSelected, startRemoveNoteSelected } from "../../actions/Note";
+import { DateUpdated } from "../ui/DateUpdated";
 
-export const NoteContent = ({note}:any) => {
+export const NoteContent = ({ note }: any) => {
 
     const dispatch = useDispatch();
 
@@ -18,8 +19,13 @@ export const NoteContent = ({note}:any) => {
             <div className="note-content">
                 <h3>{note.title}</h3>
                 <span>{note.content}</span>
+                <br />
+                {
+                    note.updatedAt &&
+                    <DateUpdated date={note.updatedAt} />
+                }
             </div>
-            <div className="note-button">
+            <div>
                 <div className="form-check">
                     <input className="form-check-input shadow-none" type="checkbox" value="" id="flexCheckDefault" onChange={(e) => handleSelected(e)} />
                 </div>
